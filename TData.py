6adaa05d61275461c9cd7deac8b1d7b6dcb597bc
adams <adamsandstrom1989@gmail.com>
2021-02-04 17:47:29 +0100

    add sum

--- TData.py
+++ TData.py
@@ -14,9 +14,10 @@ class TData:
         self.token = os.getenv('12DATA_TOKEN')
         # options: 1min, 5min, 15min, 30min, 45min, 1h, 2h, 4h, 8h, 1day, 1week, 1month
         self.options = ['1min', '5min', '15min', '30min', '45min', '1h', '2h', '4h', '8h', '1day', '1week', '1month']
+        self.summary = ['sum','summary']
 
     def get_help(self, stock):
-        return f"Usage: {stock} [" + " | ".join([option for option in self.options]) + "]"
+        return f"Usage: {stock} [" + " | ".join([option for option in self.options]) + "]" + " [sum | summary]"
 
     def get_meta(self, stock, interval=None):
         stock_json = None
@@ -26,6 +27,8 @@ class TData:
         elif interval in self.options:
             url = f'https://api.twelvedata.com/time_series?symbol={stock}&interval={interval}&apikey={self.token}'
             stock_json = requests.get(url).json()
+        elif interval in self.summary:
+            pass
         else:
             return self.get_help(stock)
 
@@ -33,9 +36,9 @@ class TData:
             tmp_str = json.dumps(stock_json["meta"], indent=4)
             return tmp_str
 
-    def get_last3(self, stock, interval=None):
+    def get_last3(self, stock, interval=None, summary=None):
         stock_json = None
-        if interval is None:
+        if interval is None or interval in self.summary:
             url = f'https://api.twelvedata.com/time_series?symbol={stock}&interval=1min&apikey={self.token}'
             stock_json = requests.get(url).json()
         elif interval in self.options:
@@ -49,17 +52,38 @@ class TData:
             newest_value = float(stock_json["values"][0]["close"])
             to_the_moon = True
             values = list()
+
             for n in range(1, 3):
                 tmp_str += json.dumps(stock_json["values"][n], indent=4)
                 values.append(float(stock_json["values"][n]["close"]))
             for value in values:
                 if value > newest_value:
                     to_the_moon = False
+                    break
             if to_the_moon:
-                return tmp_str + "\n\n :crescent_moon: TOOOO THE MOOOOOOOOON!! :partying_face: DIAMOND HAAANDZZ :partying_face: "
+                moon_str = "\n\n :crescent_moon: TOOOO THE MOOOOOOOOON!! :partying_face: DIAMOND HAAANDZZ :partying_face: "
+                if summary is not None:
+                    if summary.lower() in self.summary:
+                        return moon_str
+                if interval is not None:
+                    if interval.lower() in self.summary:
+                        return moon_str
+                    elif interval.lower() in self.options:
+                        return tmp_str + moon_str
+                return tmp_str + moon_str
             else:
-                return tmp_str +"\n\n:poop: :poop: SHIT STOCK :poop: :poop: "
+                shit_str = "\n\n:poop: :poop: SHIT STOCK :poop: :poop: "
+                if summary is not None:
+                    if summary.lower() in self.summary:
+                        return shit_str
+                if interval is not None:
+                    if interval.lower() in self.summary:
+                        return shit_str
+                    elif interval.lower() in self.options:
+                        return tmp_str + shit_str
+                return tmp_str + shit_str
+
 
 if __name__ == '__main__':
     td = TData()
-    print(td.get_last3('GME', '11min'))
+    print(td.get_last3('GME', '1min', 'sum'))
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from TData import TData
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
-
+summary = ['sum', 'summary']
 td = TData()
 
 stock_list = list()
@@ -27,10 +27,16 @@ async def on_message(message):
     commands = message.content.lower().split()
     if commands[0] in stock_list:
         if str(message.author) == 'Kaztiell#0716':
-            await message.channel.send('https://www.swish.nu/ :AlErtdocument:')
+            await message.channel.send('https://www.swish.nu/ :eyes:')
         elif len(commands) == 2:
+            if commands[1].lower() in summary:
+                await message.channel.send(td.get_last3(commands[0], commands[1]))
+                return
             meta = td.get_meta(commands[0], commands[1])
             last3 = td.get_last3(commands[0], commands[1])
+        elif len(commands) == 3 and commands[2] in ['sum', 'summary']:
+            await message.channel.send(td.get_last3(commands[0], commands[1], commands[2]))
+            return
         else:
             meta = td.get_meta(commands[0])
             last3 = td.get_last3(commands[0])
